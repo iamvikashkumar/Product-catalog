3a533aecbadd588052223416321d9fbc0891701c
vikash01437 <34088943+vikash01437@users.noreply.github.com>
2020-01-15 18:43:34 +0530

    Initial Commit for Product Catalog

---
+++ app.js
@@ -0,0 +1,41 @@
+(function (angular) {
+    'use strict';
+    var myApp = angular.module('myApp', []);
+
+    myApp.controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
+        function init() {
+            $http.get(
+                    "https://gist.githubusercontent.com/bharadwajturlapati/4e81154dbcc7d6928921b96057fc5b4a/raw/d31da32d6e5c1dd2a11968d7e94d3c60dfd50fcb/products.json"
+                )
+                .then(function (response) {
+                    $scope.data = response.data;
+                    changeObjectToArray($scope.data);
+                });
+        }
+
+        function changeObjectToArray(data) {
+            $scope.dataInArray = Object.keys(data).map(i => data[i])
+            $scope.general = $scope.dataInArray.filter(data => data.hub === 'general');
+            $scope.other = $scope.dataInArray.filter(data => data.hub !== 'general');
+        };
+
+        var modal = document.getElementById("myModal");
+
+        $scope.showProductDetails = function () {
+            modal.style.display = "block";
+        };
+
+        $scope.close = function () {
+            modal.style.display = "none";
+        };
+
+        window.onclick = function (event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        };
+
+        init();
+
+    }]);
+})(window.angular);
